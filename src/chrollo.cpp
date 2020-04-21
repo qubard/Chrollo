577e25d8dd4ca5e1506d30b2b4ccdff92abce101
qubard <qubard@gmail.com>
2020-04-20 20:16:53 -0400

    march 20th game update

--- src/chrollo.cpp
+++ src/chrollo.cpp
@@ -1,5 +1,7 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
 
+#include <iostream>
 #include <windows.h>
 #include <TlHelp32.h>
 #include <string>
@@ -8,12 +10,32 @@
 #include <algorithm>
 #include <direct.h>
 #include <unordered_map>
-#include <set>
+#include <regex>
 // We don't need detours--that's for plebs
 #include "replace.h"
 
-std::set<std::string> blacklist;
-std::string last_server_name;
+// A list of regex blacklist patterns
+std::vector<std::regex> blacklist;
+
+typedef char byte;
+
+void GetModuleBaseAddress(DWORD procId, const wchar_t* modName, uintptr_t* modBaseAddr, uintptr_t* modSize) {
+	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId);
+	if (hSnap != INVALID_HANDLE_VALUE) {
+		MODULEENTRY32 modEntry;
+		modEntry.dwSize = sizeof(modEntry);
+		if (Module32First(hSnap, &modEntry)) {
+			do {
+				if (!_wcsicmp(modEntry.szModule, modName)) {
+					*modBaseAddr = (uintptr_t)modEntry.modBaseAddr;
+					*modSize = (uintptr_t)modEntry.modBaseSize;
+					break;
+				}
+			} while (Module32Next(hSnap, &modEntry));
+		}
+	}
+	CloseHandle(hSnap);
+}
 
 uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName) {
 	uintptr_t modBaseAddr = 0;
@@ -55,7 +77,7 @@ void clear_string_ptr(char* loc, size_t size) {
 	}
 }
 
-// copy over data into the string at loc
+// Copy over data into the string at loc
 void overwrite_string_ptr(char* loc, std::string data) {
 	// It's enough to just copy over the string into loc and delimit it with a null terminator
 	for (int i = 0; i < data.size(); i++) {
@@ -63,6 +85,9 @@ void overwrite_string_ptr(char* loc, std::string data) {
 	}
 	loc[data.size()] = '\0';
 }
+
+std::vector<std::string> ignored_chars = { "!", "/", "<", ">", "|", "*", "?", ":", "\\", "\"" };
+std::string last_server_name;
 
 void __stdcall dump_script_buffer() {
 	char* filename;
@@ -74,28 +99,44 @@ void __stdcall dump_script_buffer() {
 
 	std::string script_content = read_string_from_ptr(buffer_ptr);
 	std::string script_name = read_string_from_ptr(filename);
-	std::string server_name = read_string_from_ptr((char*)(GetModuleBaseAddress(GetCurrentProcessId(), L"client.dll") + 0x6FF0A0));
+	std::string server_name = read_string_from_ptr((char*)(GetModuleBaseAddress(GetCurrentProcessId(), L"client.dll") + 0x6FF588));
 
 	// Since we don't have an init server connection hook this will run once
 	if (last_server_name != server_name) {
 		server_name.erase(std::remove_if(server_name.begin(), server_name.end(),
-			[](auto const& c) -> bool { return !std::isalnum(c) && c != ' '; }), server_name.end());
+			[](auto const& c) -> bool { return c == ':' || c =='?' || c == '|' || c == '"' || c == '\\' || c =='\/' || c == '<' || c == '>' || c == ' '; }), server_name.end());
 		last_server_name = server_name;
 	}
 
 	// dump to disk
 	std::fstream out;
 	for (int i = 0; i < script_name.length(); i++) {
-		if ((int)script_name[i] == 47) {
+		// replace \ and / with _
+		if ((int)script_name[i] == (int)'/' || (int)script_name[i] == (int)'\\') {
 			script_name[i] = '_';
 		}
 	}
 
-	if (blacklist.find(script_name) != blacklist.end()) {
-		show_message("Blacklisted " + script_name + "!" + " with size " + std::to_string(strlen(buffer_ptr)));
-		clear_string_ptr(buffer_ptr, script_content.size()); // surprised this region is writable lol
-	} else if (replace_table.find(script_name) != replace_table.end()) {
+	// Replace script if found. 
+	// Be careful, because replacing certain scripts can cause game crashes
+	if (replace_table.find(script_name) != replace_table.end()) {
 		overwrite_string_ptr(buffer_ptr, replace_table[script_name]);
+
+		if (replace_table[script_name].length() > script_content.length()) {
+			// Scripts cant be larger than what is allocated
+			show_message("Replacement script " + script_name + " is too large");
+		}
+
+		show_message("Replaced script " + script_name);
+	}
+	else { // check the blacklist
+		for (auto pattern : blacklist) {
+			if (std::regex_search(script_name, pattern)) { // Matches if any substring matches the given pattern
+				show_message("Blacklisted " + script_name + "!" + " with size " + std::to_string(strlen(buffer_ptr)));
+				clear_string_ptr(buffer_ptr, script_content.size());
+				break;
+			}
+		}
 	}
 
 	_mkdir(std::string("./chrollo/" + last_server_name).c_str());
@@ -108,12 +149,38 @@ void __stdcall dump_script_buffer() {
 	out.close();
 }
 
-typedef char byte;
-
 void hook_dump() {
-	uintptr_t base = GetModuleBaseAddress(GetCurrentProcessId(), L"lua_shared.dll");
-	uintptr_t luaJIT_version_2_0_4 = base + 0x5860;
-	uintptr_t hook_addr = luaJIT_version_2_0_4 + 0x1C28;
+	uintptr_t lua_shared_base, lua_shared_size;
+	GetModuleBaseAddress(GetCurrentProcessId(), L"lua_shared.dll", &lua_shared_base, &lua_shared_size);
+	uintptr_t luaJIT_version_2_0_4 = lua_shared_base + 0x5860;
+	
+	const int patchSize = 11;
+	byte patch[patchSize] = { 0x8B, 0xCB, 0xff, 0xd0, 0x5f, 0x5e, 0x5b, 0x5d, 0xc2, 0x14, 0x00 };
+
+	int hook_addr = -1;
+	show_message(std::to_string(lua_shared_size));
+	// `patch` is actually our signature as well
+	for (byte* addr = (byte*)luaJIT_version_2_0_4 + 0x1AC8; addr  < (byte*)luaJIT_version_2_0_4 + 0x1AC8 + patchSize; addr++) {
+		int m = 0;
+		for (int i = 0; i < patchSize; i++) {
+			if (*(addr + i) == (byte)patch[i]) {
+				m++;
+			}
+			else {
+				break;
+			}
+		}
+
+		if (m == patchSize) {
+			hook_addr = (int)addr;
+			break;
+		}
+	}
+
+	if (hook_addr < 0) {
+		show_message("Chrollo failed to find valid hook! Terminating");
+		return;
+	}
 
 	DWORD old;
 	bool success = VirtualProtectEx(GetCurrentProcess(), (uintptr_t*)hook_addr, 20, PAGE_EXECUTE_READWRITE, &old);
@@ -126,9 +193,11 @@ void hook_dump() {
 	*((uintptr_t*)patch_addr) = (uintptr_t)&dump_script_buffer - (uintptr_t)patch_addr - 0x04;
 	patch_addr += 4;
 
-	// POPAD then patch back with original instructions
-	byte patch[12] = { 0x61, 0x8B, 0xCB, 0xff, 0xd0, 0x5f, 0x5e, 0x5b, 0x5d, 0xc2, 0x14, 0x00 };
-	for (int i = 0; i < 12; i++) {
+	*((byte*)patch_addr) = 0x61; // POPAD
+	patch_addr++;
+
+	// Patch back with original instructions
+	for (int i = 0; i < patchSize; i++) {
 		patch_addr[i] = patch[i];
 	}
 
@@ -144,10 +213,11 @@ void read_blacklist(std::string root) {
 	if (file.fail()) {
 		show_message("Failed to read blacklist in " + dir);
 	}
-
-	std::string line;
-	while (getline(file, line)) {
-		blacklist.emplace(line.c_str());
+	else {
+		std::string line;
+		while (getline(file, line)) {
+			blacklist.emplace_back(line);
+		}
 	}
 }
 
@@ -173,4 +243,3 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 	}
 	return TRUE;
 }
-
